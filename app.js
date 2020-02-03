12dfb47ec48709f16cfde8bf1eac37fa113d4b0e
Your Name <marco.rainone91@gmail.com>
2020-02-03 02:01:22 +0100

    solved CORS problem

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 var uid = require('uid-safe');
-app.get( '/new-password/:user',(req,res) => {
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.get( '/new-password',(req,res) => {
 
 
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-res.statusMessage ='TODO BIEN CHICO';
-const a = req.query.id || null;
+res.statusCode = 200;
+/* const a = req.query.id || null; */
 
- return  /* res.status(status).json({idUser: uid.sync(18)}) */ res.send(
-    `PUT HTTP method on user/${req.params.user} /${req.query.name} ${req.query.surname}   resource`,
- );
+ return  res.status('200').json({idUser: uid.sync(18)})
+    /* `PUT HTTP method on user/${req.params.user} /${req.query.name} ${req.query.surname}   resource` */
+ 
 
 });
+app.post( '/new-password',(req,res) => {
+
+
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.statusMessage ='TODO BIEN CHICO';
+   const a = req.body /* || null; */
+   
+   console.log(a);
+      return res.json(a); 
+   
+   });
 
 
 app.listen(3000);
